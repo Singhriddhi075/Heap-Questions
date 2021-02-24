f0eb6c591aca11c7c394545253935ac110cbecd8
Riddhi Singh <75823967+Singhriddhi075@users.noreply.github.com>
2021-02-24 19:44:50 +0530

    Add files via upload
    heap question

---
+++ HEAP/Heap/heapclient.java
@@ -0,0 +1,33 @@
+package Heap;
+
+public class heapclient {
+
+	public static void main(String[] args) {
+		addorremove heap=new addorremove();
+		heap.add(10);
+		heap.display();
+		heap.add(20);
+		heap.display();
+		heap.add(30);
+		heap.display();
+		heap.add(40);
+		heap.display();
+		heap.add(5);
+		heap.display();
+		heap.add(3);
+		heap.display();
+		heap.remove();
+		heap.display();
+		System.out.println(heap.remove());
+		heap.display();
+		System.out.println(heap.remove());
+		heap.display();
+		System.out.println(heap.remove());
+		heap.display();
+
+
+
+
+	}
+
+}
